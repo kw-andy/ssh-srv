67bd8120a6a4eca36ed8080d59d0a06fbbb33b7f
Andy K <andy.h.kwok@gmail.com>
2018-04-15 18:26:38 +0200

    Update ssh-pex.py
    changing the IP address and putting a \ in front of the ls

--- ssh-pex.py
+++ ssh-pex.py
@@ -1,11 +1,12 @@
 from pexpect import pxssh
 conn = pxssh.pxssh()
-if not conn.login ('174.138.12.81', 'myusername', 'mypassword'):
+#changed the Ip address
+if not conn.login ('192.18.1.11', 'pi', 'raspberry'):
     print("SSH session failed on login.")
     print(str(conn))
 else:
     print("SSH session login successful")
-    conn.sendline('ls -l')
+    conn.sendline('\ls -l')
     conn.prompt()         # match the prompt
     print(conn.before)     # print everything before the prompt.
     conn.logout()
