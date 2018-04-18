3df4dbb5b8896bd04dd40f40e759e4bcd77aa4ba
kw-andy <andy.kwok.work@gmail.com>
2018-04-18 19:23:54 +0000

    MAJ ssh-pex.py

--- ssh-pex.py
+++ ssh-pex.py
@@ -1,11 +1,11 @@
 from pexpect import pxssh
 conn = pxssh.pxssh()
-if not conn.login ('174.138.12.81', 'myusername', 'mypassword'):
+if not conn.login ('192.168.1.30', 'pi', 'raspberry'):
     print("SSH session failed on login.")
     print(str(conn))
 else:
     print("SSH session login successful")
-    conn.sendline('ls -l')
+    conn.sendline('\ls -l')
     conn.prompt()         # match the prompt
     print(conn.before)     # print everything before the prompt.
     conn.logout()
