126bd37e87cd3b10998ed6084bdbf128978b3d14
kw-andy <andy.kwok.work@gmail.com>
2018-04-22 14:24:07 +0000

    modif

--- ssh-pex.py
+++ ssh-pex.py
@@ -6,20 +6,11 @@ if not conn.login ('192.168.1.10', 'pi', 'raspberry'):
     print(str(conn))
 else:
     print("SSH session login successful")
-    conn.sendline('\ls -l')
+    conn.sendline('ls -l')
+    #the prompt helps to display the command line
     conn.prompt()
-    abc = conn.before
-    print(abc.decode())
- #abc = str(conn.sendline('\ls -l'))
-    #print(abc.encode('utf-8').decode('utf-8'))
-    #print(abc.decode('utf-8'))
-#    pipe_data = conn.sendline('LC_ALL=C ls -l');print(pipe_data.decode('utf-8'))
-#    print(pipe_data)
-#   print(pdb.pm('pipe_data()')) 
-#    conn.sendline('LC_ALL=C ls -l')
-#    pipe_data = conn.read()
-#    print(pipe_data.decode('utf-8'))
-    #conn.prompt()         # match the prompt
-    #print(conn.before)     # print everything before the prompt.
+    #putting the conn.before in a variable
+    var_before = conn.before
+    print(var_before.decode())
     conn.logout()
     conn.close()
