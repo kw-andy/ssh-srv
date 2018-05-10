22c86ca949fbca054511b8f52ca798611ff29c1c
miaou <andy.kwok.work@gmail.com>
2018-05-10 10:15:11 +0200

    adding the python part, as a test

--- ssh-pex.py
+++ ssh-pex.py
@@ -10,7 +10,6 @@ else:
     #the prompt helps to display the command line
     conn.prompt()
     #putting the conn.before in a variable
-    var_before = conn.before
-    print(var_before.decode())
+    conn.before.decode()
     conn.logout()
     conn.close()
