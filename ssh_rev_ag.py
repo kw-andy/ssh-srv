e30e7be2be8968d1f9a41564fc687a507437fb5f
Andy K <andy.h.kwok@gmail.com>
2018-06-11 15:45:43 +0200

    Update ssh_rev_ag.py

--- ssh_rev_ag.py
+++ ssh_rev_ag.py
@@ -3,12 +3,14 @@ import pexpect
 
 #os.system('ssh-agent -s; ssh-add /home/miaou/priv_keys; ssh-agent bash')
 
-# using a function to do the connection and creating a file *
-# on the remote server
-# 10/06/18 : adding part of your prompt on it, the distant server 
-# prompt is 
-# [pi:/home/pi]$ 
-# o using hild.expect('pi')
+"""
+ using a function to do the connection and creating a file *
+ on the remote server
+ 10/06/18 : adding part of your prompt on it, the distant server 
+ prompt is 
+ [pi:/home/pi]$ 
+ or using child.expect('pi')
+"""
 
 
 def conn_wr():
