7c9610246ff36c42856b6b0dda9a6f7ded0b631a
Andy K <andy.kwok.work@gmail.com>
2018-06-21 23:16:45 +0200

    Update and rename ssh_rev_ag.py to conn_wr.py
    changing the name of the method to reflect the method . This method is for programs like sftp or ssh, which needs some human interactions

--- bastion tools/conn_wr.py
+++ bastion tools/conn_wr.py
@@ -4,14 +4,16 @@ import pexpect
 #os.system('ssh-agent -s; ssh-add /home/miaou/priv_keys; ssh-agent bash')
 
 """
- using a function to do the connection and creating a file *
+ creating a function, conn_wr to do the connection and creating a file 123toto.txt
  on the remote server
+ 
  10/06/18 : adding part of your prompt on it, the distant server 
  prompt is 
  [pi:/home/pi]$ 
  or using child.expect('pi')
 """
 
+# this method is for programs like sftp or ssh, which needs some human interactions
 
 def conn_wr():
     child = pexpect.spawn('ssh pi@192.168.1.32')
@@ -21,12 +23,3 @@ def conn_wr():
     child.sendline('touch 123toto.txt')
     child.expect('pi') 
     child.sendline('exit')    
-
-
-# the same (or almost) as above but without the function
-
-child = pexpect.spawn('ssh pi@192.168.1.32')
-child.expect('password:')
-child.sendline('XXX')
-child.sendline('touch 123toto.txt')
-child.sendline('exit')    
